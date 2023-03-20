765b8c990a967528132a766306ba2b71590f83d1
HaydenLenca <120521124+HaydenLenca@users.noreply.github.com>
2023-03-20 14:00:49 -0700

    adding code to js and updated README

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,27 @@
-var search = $('button');
-var pastSearches = '';
+var myAPIKey = '2f0ac3b71bca0bcab4177e1d01bb9916';
+var searchBtn = document.getElementById("#searchBtn");
+var city = document.getElementById("city").value;
+var cities = [];
+var lon = [];
+var lat = [];
+var today = dayjs();
+
+console.log(city.value);
+
+function saveCity(city) {
+    city.push(cities);
+    localStorage.setItem("Searched",JSON.stringify(cities));
+    console.log(cities);
+    
+}
+
+
+function searchCity(event) {
+    event.preventDefault();
+    cityDisplay = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + myAPIKey + "&units=metric";
+    fetch ("https://api.openweathermap.org/data/2.5/weather?q=")
+    .then(function (response) {
+        return response.json();
+    })
+};    
+
