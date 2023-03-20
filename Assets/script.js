a225ab257c82241ace31ca511426fda421d6a23c
HaydenLenca <120521124+HaydenLenca@users.noreply.github.com>
2023-03-20 15:07:07 -0700

    working on localStorage

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,31 @@
-var myAPIKey = '2f0ac3b71bca0bcab4177e1d01bb9916';
-var searchBtn = document.getElementById("#searchBtn");
-var city = document.getElementById("city").value;
+var APIKey = '2f0ac3b71bca0bcab4177e1d01bb9916';
+var searchBtn = document.getElementById('searchBtn');
+var city = document.querySelector('#city').value;
+var input = document.querySelector('input');
 var cities = [];
 var lon = [];
 var lat = [];
-var today = dayjs();
+var today = dayjs('MMM D, YYYY');
+test = city.value;
+console.log(city);
+console.log(cities); 
 
-console.log(city.value);
 
-function saveCity(city) {
+input.addEventListener("click", function() {
     city.push(cities);
     localStorage.setItem("Searched",JSON.stringify(cities));
-    console.log(cities);
-    
-}
+    console.log(cities);  
+});
+
 
 
 function searchCity(event) {
     event.preventDefault();
-    cityDisplay = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + myAPIKey + "&units=metric";
+    cityDisplay = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     fetch ("https://api.openweathermap.org/data/2.5/weather?q=")
     .then(function (response) {
         return response.json();
     })
-};    
+};  
+searchCity();  
 
